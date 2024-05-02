5a4c2a9379c8acf5bedabece46284d234e84c33a
Vecho05 <119891421+Vecho05@users.noreply.github.com>
2024-05-02 11:34:05 +0200

    tabellaTecnico

--- GESTIONE_TICKET_2_05_2024/GestioneTicket.sql
+++ GESTIONE_TICKET_2_05_2024/GestioneTicket.sql
@@ -1,2 +1,14 @@
 -- CIAO LOLLIO
 --CIAO PEDRO
+
+
+CREATE TABLE Tecnico(
+    IDTecnico INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50),
+    Cognome VARCHAR(50),
+    DataNascita DATE,
+    Email VARCHAR(50),
+    Telefono VARCHAR(10)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+)ENGINE=InnoDB;
