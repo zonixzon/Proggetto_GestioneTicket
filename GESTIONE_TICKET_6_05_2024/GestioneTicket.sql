1dd001d2827e9f594b49e27b3bbb45cca18a4287
Vecho05 <119891421+Vecho05@users.noreply.github.com>
2024-05-06 08:31:19 +0200

    MOD4_GestioneTicket

--- GESTIONE_TICKET_6_05_2024/GestioneTicket.sql
+++ GESTIONE_TICKET_6_05_2024/GestioneTicket.sql
@@ -4,7 +4,7 @@ CREATE TABLE Utente(
     Cognome VARCHAR(30),
     DataNascita DATE,
     Email VARCHAR(50),
-    Telefono VARCHAR(50)
+    Telefono VARCHAR(20)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 )ENGINE=InnoDB;
@@ -15,32 +15,43 @@ CREATE TABLE Tecnico(
     Cognome VARCHAR(50),
     DataNascita DATE,
     Email VARCHAR(50),
-    Telefono VARCHAR(10)
+    Telefono VARCHAR(20)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 )ENGINE=InnoDB;
 
 CREATE TABLE CategoriaProblema(
-    IDCategoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    IDCategoria INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     NomeCategoria VARCHAR(50),
-    Descrizione VARCHAR(200)
+    Descrizione VARCHAR(500)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 )ENGINE=InnoDB;
 
+CREATE TABLE Posizione(
+    IDPosizione INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NomeReparto VARCHAR(40),
+    Piano INT(1), -- DA CONTROLLARE
+    NumeroUfficio INT(1)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+
 CREATE TABLE Ticket(
     IDTicket INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     DescrizioneProblema VARCHAR(500),
-    Stato 
-    DATA DATE,
+    Stato ENUM('Aperto','Chiuso','In attesa')--DA CONTROLLARE
+    Data DATE,
     Assegnato BOOLEAN,
     IDUtente INT(6),
     IDTecnico INT(6),
     IDCategoria INT(6),
     IDPosizione INT(6),
-
     FOREIGN KEY(IDUtente) REFERENCES Utente(IDUtente),
-    FOREIGN KEY(IDTecnico) REFERENCES Tecnico(IDUtente)
+    FOREIGN KEY(IDTecnico) REFERENCES Tecnico(IDTecnico),
+    FOREIGN KEY(IDCategoria) REFERENCES CategoriaProblema(IDCategoria),
+    FOREIGN KEY(IDPosizione) REFERENCES Posizione(IDPosizione)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 )ENGINE=InnoDB;
